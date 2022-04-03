4ed805afbbf67302c592ebde0aee641b1941eefb
ondrejjirchar <92851722+ondrejjirchar@users.noreply.github.com>
2022-04-04 00:41:00 +0200

    Add files via upload

---
+++ Demo program/age.php
@@ -0,0 +1,69 @@
+<?php
+class Person
+{
+  protected $name;
+  protected $age;
+ 
+  public function getName()
+  {
+    return $this->name;
+  }
+ 
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+ 
+  private function callToPrivateNameAndAge()
+  {
+    return "{$this->name} is {$this->age} years old.";
+  }
+ 
+  protected function callToProtectedNameAndAge()
+  {
+    return "{$this->name} is {$this->age} years old.";
+  }
+}
+ 
+class Employee extends Person
+{
+  private $designation;
+  private $salary;
+ 
+  public function getAge()
+  {
+    return $this->age;
+  }
+ 
+  public function setAge($age)
+  {
+    $this->age = $age;
+  }
+ 
+  public function getDesignation()
+  {
+    return $this->designation;
+  }
+ 
+  public function setDesignation($designation)
+  {
+    $this->designation = $designation;
+  }
+ 
+  public function getSalary()
+  {
+    return $this->salary;
+  }
+ 
+  public function setSalary($salary)
+  {
+    $this->salary = $salary;
+  }
+ 
+  public function getNameAndAge()
+  {
+    return $this->callToProtectedNameAndAge();
+  }
+}
+ 
+$employee = new Employee();
